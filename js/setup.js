0863cee1436298b9f26afb91c9f83547d8a98a97
Thiago_Miranda <31423420+thiago-miranda@users.noreply.github.com>
2021-09-29 13:40:52 -0300

    Add files via upload

---
+++ js/setup.js
@@ -0,0 +1,18 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            item:1
+        },
+        600:{
+            item:3
+        },
+        1000:{
+            item:5
+        }
+    }
+})
+
+$(".js-modal-btn").modalVideo();
